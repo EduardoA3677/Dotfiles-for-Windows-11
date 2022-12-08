ad69216077eaa68c2355733f400690d6f215948f
DakineMI <jonathan.m.hughes.1@gmail.com>
2022-12-08 13:23:37 -0500

    wip

--- src/VSCode/VSCode.ps1
+++ src/VSCode/VSCode.ps1
@@ -13,18 +13,28 @@ function Set-VSCode-Configuration {
 choco install -y "vscode" --params "/NoDesktopIcon /NoQuicklaunchIcon /NoContextMenuFiles /NoContextMenuFolders";
 Set-VSCode-Configuration;
 refreshenv;
-code --install-extension "ue.alphabetical-sorter";
-code --install-extension "formulahendry.auto-rename-tag";
-code --install-extension "ms-azuretools.vscode-docker";
-code --install-extension "usernamehw.errorlens";
-code --install-extension "eamodio.gitlens";
-code --install-extension "oderwat.indent-rainbow";
-code --install-extension "davidanson.vscode-markdownlint";
-code --install-extension "robole.markdown-snippets";
-code --install-extension "pkief.material-icon-theme";
-code --install-extension "ms-vscode.powershell";
-code --install-extension "esbenp.prettier-vscode";
-code --install-extension "ms-vscode-remote.remote-containers";
-code --install-extension "ms-vscode-remote.remote-wsl";
-code --install-extension "jock.svg";
-code --install-extension "rangav.vscode-thunder-client";
+# Formatting and Rules
+code-insiders --install-extension dbaeumer.vscode-eslint --force
+code-insiders --install-extension esbenp.prettier-vscode --force
+code-insiders --install-extension aaron-bond.better-comments --force
+# HTML and CSS
+code-insiders --install-extension formulahendry.auto-rename-tag --force
+code-insiders --install-extension naumovs.color-highlight --force
+code-insiders --install-extension anteprimorac.html-end-tag-labels --force
+# Git
+code-insiders --install-extension github.vscode-pull-request-github --force
+code-insiders --install-extension eamodio.gitlens-insiders --force
+# Angular
+code-insiders --install-extension angular.ng-template --force
+# .NET
+code-insiders --install-extension ms-dotnettools.csharp --force
+code-insiders --install-extension visualstudioexptteam.vscodeintellicode --force
+# Test Runners
+code-insiders --install-extension ms-playwright.playwright --force
+# Markdown
+code-insiders --install-extension yzhang.markdown-all-in-one --force
+code-insiders --install-extension davidanson.vscode-markdownlint --force
+# Rest Client
+code-insiders --install-extension rangav.vscode-thunder-client --force
+# Miscellaneous
+code-insiders --install-extension github.copilot --force
