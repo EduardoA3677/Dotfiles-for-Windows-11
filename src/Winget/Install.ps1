b861c578c2e323a6a8f1ca02e401435264a0bc5f
dakinemi <jonathan.m.hughes.1@gmail.com>
2022-12-08 22:37:04 -0500

    wip

--- src/Winget/Install.ps1
+++ src/Winget/Install.ps1
@@ -1,3 +1,42 @@
+function WingetInstallPackage(
+    
+    [CmdletBinding()]
+    [Alias("WIP")]
+    [Parameter(Position = 0)][System.String]$package)
+{
+    $item = $wingetList | Where-Object IdPrefix -eq $package 
+    $status = if($item) {if($item.Available){'Upgrade'}else{'Current'}} else {'Install'}
+    if($status -eq 'Current')
+    {
+        Write-Host -Fore Yellow ` ($package + ' installed at current version')
+    }
+    if($status -eq 'Install')
+    {
+        Write-Host -Fore Yellow ` ('Installing ' + $package)
+        winget install $package -h --exact --accept-source-agreements --accept-package-agreements --force
+    }
+    if($status -eq 'Upgrade') 
+    {
+        Write-Host -Fore Yellow ` ('Upgrading ' + $package)
+        winget upgrade $package -h --exact --accept-source-agreements --accept-package-agreements --include-unknown --force
+    }
+}
+Set-Alias -Name "WIP" -Value "WingetInstallPackage";
+
+function WingetUninstallPackage(
+
+    [CmdletBinding()]
+    [Alias("WUIP")]
+    [Parameter(Position = 0)][System.String]$package)
+{
+    if(winget list --exact -q=$package) 
+    {
+        Write-Host -Fore Yellow ` ('Uninstalling ' + $package)
+        winget uninstall $package -h --exact 
+    }
+}
+Set-Alias -Name "WUIP" -Value "WingetUninstallPackage";
+
 winget source list
 winget source update
 $wingetList = Invoke-WingetList
