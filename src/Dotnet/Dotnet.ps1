774a977c62b61e155aa82be2bc7f0128493c41d9
dakinemi <jonathan.m.hughes.1@gmail.com>
2022-12-08 22:24:53 -0500

    WIP
    Signed-off-by: dakinemi <jonathan.m.hughes.1@gmail.com>

--- src/Dotnet/Dotnet.ps1
+++ src/Dotnet/Dotnet.ps1
@@ -1,8 +1,2 @@
 choco install -y "dotnet-sdk";
 refreshenv;
-dotnet tool install --global dotnet-ef;
-dotnet tool install --global dotnet-update-all-tools;
-dotnet tool install --global dotnet-codegencs;
-dotnet tool install --global jetbrains.resharper.globaltools;
-dotnet tool install --global upgrade-assistant;
-dotnet tool install --global dotnet-outdated-tool;
--- src/VS/Studio.ps1
+++ src/VS/Studio.ps1
@@ -2,3 +2,10 @@ $files = Get-ChildItem -Path ",\" -Include "*.json" -Recurse
 ForEach($file in $files) {
     winget install --accept-package-agreements --accept-source-agreements $file.FullName
 }
+
+dotnet tool install --global dotnet-ef;
+dotnet tool install --global dotnet-update-all-tools;
+dotnet tool install --global dotnet-codegencs;
+dotnet tool install --global jetbrains.resharper.globaltools;
+dotnet tool install --global upgrade-assistant;
+dotnet tool install --global dotnet-outdated-tool;
